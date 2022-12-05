fefe3e5da711aaadd6c7f2f908883e65d235e617
Justin <kuoyiting123@gmail.com>
2022-12-05 13:55:43 +0800

    Delete tkinter

--- main.py
+++ main.py
@@ -20,8 +20,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField, CKEditor
-import tkinter as tk
-from tkinter import messagebox
+
 
 
 
